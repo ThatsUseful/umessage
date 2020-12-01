f3e8b73704837932c69efce9f164539294f771a8
Scott Rutherford <scott@Mac-Mini.local>
2020-12-01 02:35:43 -0800

    more

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -14,18 +14,70 @@ module ChatsHelper
   def attachment_plist_tag(data)
     cf_list = CFPropertyList::List.new(formatted: true)
     cf_list.load_binary_str(data)
+
     logger.info("New attachment")
-    logger.info(cf_list.value.value["$objects"].value[6].value)
-    logger.info(cf_list.value.value["$objects"].value[7].value)
-    logger.info(cf_list.value.value["$objects"].value[4].value)
-    logger.info(cf_list.value.value["$objects"].value[15].value)
-    logger.info(cf_list.value.value["$objects"].value[21].value)
+
+    # Get the objects
+    objects = cf_list.value.value["$objects"].value
+    
+    # Get the root node
+    top = cf_list.value.value["$top"]
+    root_node = top.value["root"]
+    root_data_idx = root_node.value
+
+    # Get the root data node
+    root_data_node = objects[root_data_idx].value
+
+    # Find out where the metadata is
+    metadata_node = root_data_node["richLinkMetadata"].value
+
+    # Get the metadata node
+    metadata = objects[metadata_node]
+
+    # Flatten
+    flattened = flatten_object(objects, metadata)
+
+    logger.info(flattened.inspect)
 
     # content_tag('pre', cf_list.to_str(CFPropertyList::List::FORMAT_XML), class: "list-group-item")
-    content_tag('pre', cf_list.to_str(CFPropertyList::List::FORMAT_PLAIN), class: "list-group-item")
+    content_tag('pre', flattened, class: "list-group-item")
 
     # text = cf_list.
     # auto_link(simple_format(text), html: { target: "_blank" })
+  end
+
+  def flatten_object(objects, object)
+    return object.value if object.is_a?(CFPropertyList::CFString)
+
+    # If there is a CFUid, recurse that entry. Else, if not, this is the end
+    if object.is_a?(CFPropertyList::CFDictionary) then
+      dict = object.value
+      dict.each_pair do |k,v|
+        if k.match(/class/) then
+          dict.delete(k)
+
+        elsif v.is_a?(CFPropertyList::CFUid) then
+          dict[k] = flatten_object(objects, objects[v.value])
+
+        elsif v.is_a?(CFPropertyList::CFString) then
+          dict[k] = v.value
+
+        elsif v.is_a?(CFPropertyList::CFInteger) then
+          dict[k] = v.value
+
+        elsif v.is_a?(CFPropertyList::CFDictionary) then
+          dict[k] = flatten_object(objects, v)
+
+        elsif v.is_a?(CFPropertyList::CFArray) then
+          dict[k] = flatten_object(objects, v)
+        end
+      end
+
+    elsif object.is_a?(CFPropertyList::CFArray) then
+      object.value.each_with_index do |item, idx|
+        object.value[idx] = flatten_object(objects, item)
+      end
+    end
   end
 
   def datetime(datetime)
